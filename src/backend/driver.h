b28de313ed91bb4c46138ab20c5e84a896b2c444
YoYoYonnY <yoyoyonny@gmail.com>
2020-02-15 01:32:32 +0100

    Future-proof driver.h
    Add macro generator and use hexadecimal bitmasks

--- src/backend/driver.h
+++ src/backend/driver.h
@@ -13,19 +13,24 @@ struct backend_base;
 // *  NVIDIA driver doesn't like seeing the same pixmap under different
 //    ids, so avoid naming the pixmap again when it didn't actually change.
 
+#define DEFINE_DRIVERS(DEFINE_DRIVER) \
+	DEFINE_DRIVER(DRIVER_AMDGPU,      0x01, "AMDGPU") /* AMDGPU for DDX, radeonsi for OpenGL */ \
+	DEFINE_DRIVER(DRIVER_RADEON,      0x02, "Radeon") /* ATI for DDX, mesa r600 for OpenGL */ \
+	DEFINE_DRIVER(DRIVER_FGLRX,       0x04, "fglrx") \
+	DEFINE_DRIVER(DRIVER_NVIDIA,      0x08, "NVIDIA") \
+	DEFINE_DRIVER(DRIVER_NOUVEAU,     0x10, "nouveau") \
+	DEFINE_DRIVER(DRIVER_INTEL,       0x20, "Intel") \
+	DEFINE_DRIVER(DRIVER_MODESETTING, 0x40, "modesetting")
+
 /// A list of possible drivers.
 /// The driver situation is a bit complicated. There are two drivers we care about: the
 /// DDX, and the OpenGL driver. They are usually paired, but not always, since there is
 /// also the generic modesetting driver.
 /// This enum represents _both_ drivers.
 enum driver {
-	DRIVER_AMDGPU = 1, // AMDGPU for DDX, radeonsi for OpenGL
-	DRIVER_RADEON = 2, // ATI for DDX, mesa r600 for OpenGL
-	DRIVER_FGLRX = 4,
-	DRIVER_NVIDIA = 8,
-	DRIVER_NOUVEAU = 16,
-	DRIVER_INTEL = 32,
-	DRIVER_MODESETTING = 64,
+#define DEFINE_DRIVER(enumname, mask, name) enumname = mask,
+	DEFINE_DRIVERS(DEFINE_DRIVER)
+#undef DEFINE_DRIVER
 };
 
 /// Return a list of all drivers currently in use by the X server.
@@ -34,26 +39,9 @@ enum driver detect_driver(xcb_connection_t *, struct backend_base *, xcb_window_
 
 // Print driver names to stdout, for diagnostics
 static inline void print_drivers(enum driver drivers) {
-	if (drivers & DRIVER_AMDGPU) {
-		printf("AMDGPU, ");
-	}
-	if (drivers & DRIVER_RADEON) {
-		printf("Radeon, ");
-	}
-	if (drivers & DRIVER_FGLRX) {
-		printf("fglrx, ");
-	}
-	if (drivers & DRIVER_NVIDIA) {
-		printf("NVIDIA, ");
-	}
-	if (drivers & DRIVER_NOUVEAU) {
-		printf("nouveau, ");
-	}
-	if (drivers & DRIVER_INTEL) {
-		printf("Intel, ");
-	}
-	if (drivers & DRIVER_MODESETTING) {
-		printf("modesetting, ");
-	}
-	printf("\b\b \n");
+	const char *prefix = "";
+#define DEFINE_DRIVER(enumname, mask, name) if (drivers & mask) { printf("%s%s", prefix, name); prefix = ", "; }
+	DEFINE_DRIVERS(DEFINE_DRIVER)
+#undef DEFINE_DRIVER
+	printf("\n");
 }
